8104604c278a096d0ce916420804f5386415eb80
fakhrishahab <fakhri1804@gmail.com>
2016-02-03 17:31:20 +0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,10 @@ var gulp = require('gulp'),
 
 var defaultTasks = [
 	'html_view',
-	'style_sass'
+	'style_sass',
+	'fonts',
+	'script',
+	'images'
 ]
 
 gulp.task('html_view', function(){
@@ -21,6 +24,21 @@ gulp.task('style_sass', function(){
 		.pipe(gulp.dest('public/assets/./styles'))
 })
 
+gulp.task('fonts', function(){
+	return gulp.src('./dev/assets/fonts/**/*.*')
+		.pipe(gulp.dest('public/assets/fonts'))
+})
+
+gulp.task('script', function(){
+	return gulp.src('./dev/assets/script/**/*.*')
+		.pipe(gulp.dest('public/assets/script'))
+})
+
+gulp.task('images', function(){
+	return gulp.src('./dev/assets/images/**/*.*')
+		.pipe(gulp.dest('public/assets/images'))
+})
+
 gulp.task('clean', function(cb){
 	del(defaultTasks, cb)
 })
@@ -28,6 +46,7 @@ gulp.task('clean', function(cb){
 gulp.task('watch', function(){
 	gulp.watch('./dev/app/**/*.*', ['html_view'])
 	gulp.watch('./dev/assets/styles/scss/**/*.scss', ['style_sass'])
+	gulp.watch('./dev/assets/**/*.*', ['fonts', 'script', 'images'])
 })
 
 gulp.task('default', ['clean'])
