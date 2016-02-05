2034d4f1164cc06c34ad7479e828caf94f089fd0
fakhrishahab <fakhri1804@gmail.com>
2016-02-05 17:57:26 +0700

    update zeeza

--- dev/script/script.js
+++ dev/script/script.js
@@ -1,18 +1,58 @@
+function $params(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function $params2(val) {
+    var result = "Params not found",
+        tmp = [];
+    var items = location.search.substr(1).split("&");
+    for (var index = 0; index < items.length; index++) {
+        tmp = items[index].split("=");
+        if (tmp[0] === val) result = decodeURIComponent(tmp[1]);
+    }
+    return result;
+}
+
+function mobileAdapt(){
+	var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;	
+
+	if(w < 540){
+		$('#section-category').insertAfter('#section-product')
+	}else{
+		$('#section-product').insertAfter('#section-category')
+	}
+}
+
 $(document).ready(function(){
-	$('body').click(function(e){
+	mobileAdapt()
+	$('body').click(function(e){		
 		e.stopPropagation();
-		e.preventDefault();
 		$('.navbar-wrapper').removeClass('navbar-active');	
+		$(this).removeClass('overflow-hide')
 	})
 
 	$('.menu-toggle').on('click', function(e){
 		e.stopPropagation();
-		e.preventDefault();
 		$('.navbar-wrapper').toggleClass('navbar-active');
+		$('body').toggleClass('overflow-hide')
+	})
+
+	$('.navbar-wrapper').click(function(e){
+		e.stopPropagation();
 	})
 
 	$('.navbar-category ul li').on('click', function(){		
 		$(this).find('ul').toggleClass('active')
-		// $(this).find('ul').css('display','block')
-	})
+	})	
+})
+
+$(window).bind('resize',function(){
+	console.log('yes')
+	mobileAdapt()
 })
